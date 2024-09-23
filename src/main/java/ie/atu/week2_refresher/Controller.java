ae0a9dd90a8a4be37aa6fbc5c01bc9734e561438
Just0jo <G00407342@atu.ie>
2024-09-23 13:24:02 +0100

    post and get

---
+++ src/main/java/ie/atu/week2_refresher/Controller.java
@@ -0,0 +1,23 @@
+package ie.atu.week2_refresher;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("Product")
+public class Controller {
+    private List<Product> ProductList = new ArrayList<>();
+    @GetMapping("/ProductGet")
+    public List<Product> getProductList()
+    {
+        return ProductList;
+
+    }
+    @PostMapping("ProductPost")
+     public Product addProduct(@RequestBody Product product) {
+        ProductList.add(product);
+        return product;
+    }
+
+}
