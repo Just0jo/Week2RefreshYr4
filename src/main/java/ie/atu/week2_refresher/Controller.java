8c0da824045fafb7f0e1adacf42bd71f8e465fc7
Just0jo <G00407342@atu.ie>
2024-09-23 13:55:57 +0100

    SERVICE

---
+++ src/main/java/ie/atu/week2_refresher/Controller.java
@@ -0,0 +1,23 @@
+package ie.atu.week2_refresher;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+
+@RequestMapping("Product")
+public class Controller {
+    private ProductService productService = new ProductService();
+    @GetMapping("/ProductGet")
+    public List<Product> getProductList()
+    {
+        return productService.getAllProducts();
+
+    }
+    @PostMapping("ProductPost")
+    public String addProduct(@RequestBody Product product) {
+       return productService.addProduct(product);
+    }
+
+}
