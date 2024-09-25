ca58708d5ee3b26841cdd225836c7858f5592d2a
Just0jo <G00407342@atu.ie>
2024-09-25 14:01:36 +0100

    injection constructor

--- src/main/java/ie/atu/week2_refresher/Controller.java
+++ src/main/java/ie/atu/week2_refresher/Controller.java
@@ -1,14 +1,21 @@
 package ie.atu.week2_refresher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 
 @RequestMapping("Product")
 public class Controller {
-    private ProductService productService = new ProductService();
+    private final ProductService productService;
+
+    @Autowired
+    public Controller(ProductService productService) {
+        this.productService = productService;
+    }
+
+
     @GetMapping("/ProductGet")
     public List<Product> getProductList()
     {
@@ -18,5 +25,6 @@ public class Controller {
     @PostMapping("ProductPost")
     public String addProduct(@RequestBody Product product) {
        return productService.addProduct(product);
+
     }
 }
